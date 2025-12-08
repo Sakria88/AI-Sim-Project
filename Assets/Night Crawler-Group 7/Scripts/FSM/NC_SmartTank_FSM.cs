72783ce0495f51084a4df38904f17a45fa9e8c15
Sakria88 <sakriaobido@gmail.com>
2025-12-08 15:22:56 +0000

    Added needed variables

--- Assets/Night Crawler-Group 7/Scripts/FSM/NC_SmartTank_FSM.cs
+++ Assets/Night Crawler-Group 7/Scripts/FSM/NC_SmartTank_FSM.cs
@@ -13,4 +13,110 @@ public class NC_SmartTank_FSM : AITank
     public override void AITankUpdate()
     {
     }
+    public override void AIOnCollisionEnter(Collision collision)
+    {
+    }
+    public void GeneratePathToWorldPoint(GameObject pointInWorld)
+    {
+        a_FindPathToPoint(pointInWorld);
+    }
+    public void GeneratePathToWorldPoint(GameObject pointInWorld, HeuristicMode heuristic)
+    {
+        a_FindPathToPoint(pointInWorld, heuristic);
+    }
+    public void FollowPathToWorldPoint(GameObject pointInWorld, float normalizedSpeed)
+    {
+        a_FollowPathToPoint(pointInWorld, normalizedSpeed);
+    }
+    public void FollowPathToWorldPoint(GameObject pointInWorld, float normalizedSpeed, HeuristicMode heuristic)
+    {
+        a_FollowPathToPoint(pointInWorld, normalizedSpeed, heuristic);
+    }
+public void FollowPathToRandomWorldPoint(float normalizedSpeed)
+    {
+        a_FollowPathToRandomPoint(normalizedSpeed);
+    }
+
+    public void FollowPathToRandomWorldPoint(float normalizedSpeed, HeuristicMode heuristic)
+    {
+        a_FollowPathToRandomPoint(normalizedSpeed, heuristic);
+    }
+
+    public void GenerateNewRandomWorldPoint()
+    {
+        a_GenerateRandomPoint();
+    }
+     public void TankStop()
+    {
+        a_StopTank();
+    }
+     public void TankGo()
+    {
+        a_StartTank();
+    }
+    public void TurretFaceWorldPoint(GameObject pointInWorld)
+    {
+        a_FaceTurretToPoint(pointInWorld);
+    }
+    public void TurretReset()
+    {
+        a_ResetTurret();
+    }
+    public void TurretFireAtPoint(GameObject pointInWorld)
+    {
+        a_FireAtPoint(pointInWorld);
+    }
+    public bool TankIsFiring()
+    {
+        return a_IsFiring;
+    }
+    public float TankCurrentHealth
+    {
+        get
+        {
+            return a_GetHealthLevel;
+        }
+    }
+    public float TankCurrentAmmo
+    {
+        get
+        {
+            return a_GetAmmoLevel;
+        }
+    }
+    public float TankCurrentFuel
+    {
+        get
+        {
+            return a_GetFuelLevel;
+        }
+    }
+    public List<GameObject> MyBases
+    {
+        get
+        {
+            return a_GetMyBases;
+        }
+    }
+    public Dictionary<GameObject, float> VisibleEnemyTanks
+    {
+        get
+        {
+            return a_TanksFound;
+        }
+    }
+    public Dictionary<GameObject, float> VisibleConsumables
+    {
+        get
+        {
+            return a_ConsumablesFound;
+        }
+    }
+    public Dictionary<GameObject, float> VisibleEnemyBases
+    {
+        get
+        {
+            return a_BasesFound;
+        }
+    }
 }
