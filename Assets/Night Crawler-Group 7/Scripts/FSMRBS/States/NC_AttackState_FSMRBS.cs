10a95857047658fdd0b90b704e60618f9d05f4e1
Gurjot <gurjot200@hotmail.com>
2025-12-11 17:01:58 +0000

    attack state fsmrbs

--- Assets/Night Crawler-Group 7/Scripts/FSMRBS/States/NC_AttackState_FSMRBS.cs
+++ Assets/Night Crawler-Group 7/Scripts/FSMRBS/States/NC_AttackState_FSMRBS.cs
@@ -1,60 +1,67 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEngine.GraphicsBuffer;
-using static UnityEngine.UI.Selectable;
 
-public class NC_AttackState_FSM : NC_BaseState_FSM
+public class NC_AttackState_FSMRBS : NC_BaseState_FSMRBS
 {
-    private NC_SmartTank_FSM nC_SmartTank_FSM;
+    private NC_SmartTank_FSMRBS nC_SmartTank_FSMRBS;
+    private const float closeRange = 30f;   // rule threshold
 
-    public NC_AttackState_FSM(NC_SmartTank_FSM nC_SmartTank_FSM)
+    public NC_AttackState_FSMRBS(NC_SmartTank_FSMRBS nC_SmartTank_FSMRBS)
     {
-        this.nC_SmartTank_FSM = nC_SmartTank_FSM;
+        this.nC_SmartTank_FSMRBS = nC_SmartTank_FSMRBS;
     }
 
     public override Type StateEnter()
     {
-        Debug.Log("Entering the attack state FSM");
+        Debug.Log("Entering the attack state FSMRBS");
         return null;
     }
 
     public override Type StateUpdate()
     {
-        if (nC_SmartTank_FSM.TankCurrentHealth < 35f)
+        // Safety checks first
+        if (nC_SmartTank_FSMRBS.TankCurrentHealth < 35f)
+            return typeof(NC_RetreatState_FSMRBS);
+
+        if (nC_SmartTank_FSMRBS.TankCurrentFuel < 35f || nC_SmartTank_FSMRBS.TankCurrentAmmo == 0)
+            return typeof(NC_ScavengeState_FSMRBS);
+
+        GameObject target = nC_SmartTank_FSMRBS.NCEnTank;
+
+        // No target means leave attack
+        if (target == null)
+            return typeof(NC_PatrolState_FSMRBS);
+
+        // Check if visible enemy + distance
+        Dictionary<GameObject, float> vis = nC_SmartTank_FSMRBS.VisibleEnemyTanks;
+
+        bool enemyInSight = vis != null && vis.ContainsKey(target);
+        if (!enemyInSight)
+            return typeof(NC_PatrolState_FSMRBS);
+
+        float enemyDistance = vis[target];
+
+        
+        // RULE:
+        // if (enemyInSight && enemyDistance <= closeRange)
+        //      nextState = Attack  (so: stay here and attack)
+        
+        if (enemyDistance <= closeRange)
         {
-            return typeof(NC_RetreatState_FSM);
+            // Aim + fire
+            nC_SmartTank_FSMRBS.TurretFaceWorldPoint(target);
+            nC_SmartTank_FSMRBS.TurretFireAtPoint(target);
+            return null; // stay in Attack
         }
-        else if (nC_SmartTank_FSM.TankCurrentFuel < 35f || nC_SmartTank_FSM.TankCurrentAmmo == 0)
-        {
-            return typeof(NC_ScavengeState_FSM);
-        }
-        else if (nC_SmartTank_FSM.NCEnTank != null)
-        {
-            float distance = Vector3.Distance(nC_SmartTank_FSM.transform.position, nC_SmartTank_FSM.NCEnTank.transform.position);
-            if (distance > 30f)
-            {
-                return typeof(NC_PursueState_FSM);
-            }
-            else
-            {
-                // Attack logic
-                nC_SmartTank_FSM.TurretFireAtPoint(nC_SmartTank_FSM.NCEnTank);
-            }
-        }
-        else
-        {
-            return typeof(NC_PatrolState_FSM);
-        }
-        return null;
+
+        // Otherwise, pursue
+        return typeof(NC_PursueState_FSMRBS);
     }
 
     public override Type StateExit()
     {
-        Debug.Log("Exiting the attack state");
+        Debug.Log("Exiting the attack state FSMRBS");
         return null;
     }
 }
-
